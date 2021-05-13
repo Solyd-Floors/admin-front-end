276bc589e0a383c2c12ec444e21b43449fcd687d
Gjergj Kadriu <gjergjk71@gmail.com>
2021-05-14 01:51:49 +0200

    PUSH

--- src/configs.js
+++ src/configs.js
@@ -1,3 +1,11 @@
 
-export const API_ENDPOINT = "https://solyd-floors-back.herokuapp.com/api/v1"
-// export const API_ENDPOINT = "http://localhost:4000/api/v1"
+let base_url;
+
+if (window.location.href.indexOf("localhost") === -1){
+    base_url = "http://localhost:4000/api/v1"
+} else {
+    base_url = "https://solyd-floors-back.herokuapp.com/api/v1"
+}
+
+
+export const baseURL = base_url;
