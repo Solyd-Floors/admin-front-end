98475ccddda97549e70724201ed849615e7e2aee
Gjergj Kadriu <gjergjk71@gmail.com>
2021-05-14 01:52:25 +0200

    PUSH

--- src/configs.js
+++ src/configs.js
@@ -1,7 +1,7 @@
 
 let base_url;
 
-if (window.location.href.indexOf("localhost") === -1){
+if (window.location.href.indexOf("localhost") !== -1){
     base_url = "http://localhost:4000/api/v1"
 } else {
     base_url = "https://solyd-floors-back.herokuapp.com/api/v1"
