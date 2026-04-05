70cf478e04142f44cdfc24d2e98f5e6c894aad34
TSArchghs's mod <gjergjk71@gmail.com>
2026-04-05 17:18:15 +0200

    Update configs.js

--- src/configs.js
+++ src/configs.js
@@ -2,9 +2,9 @@
 let base_url;
 
 if (window.location.href.indexOf("localhost") !== -1){
-    base_url = "http://localhost:4000/api/v1"
+    base_url = "https://solyd-floors-back-end.vercel.app/api/v1"
 } else {
-    base_url = "https://solyd-floors-back.herokuapp.com/api/v1"
+    base_url = "https://solyd-floors-back-end.vercel.app/api/v1"
 }
 
 
